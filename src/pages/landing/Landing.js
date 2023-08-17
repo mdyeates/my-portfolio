2426a9fcc2d5d571f9250dd8424bcc9916b9ed75
Michael Yeates <michael-yeates@outlook.com>
2023-08-17 10:43:23 +0100

    Add spring transition to name

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -48,7 +48,7 @@ const Landing = ({ name }) => {
           style={styles.name}
           initial={{ y: -100, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.6, ease: "easeInOut" }}
+          transition={{ delay: 0.1, duration: 0.6, type: "spring" }}
         >
           {name}
         </motion.h1>
