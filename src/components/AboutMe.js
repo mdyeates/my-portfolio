cbc91f15ddaa19fad9403c50b02a2461752c326d
Michael Yeates <michael-yeates@outlook.com>
2023-08-18 17:50:36 +0100

    Remove delay from about photo

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -34,7 +34,7 @@ const AboutMe = ({ name }) => {
             ref={ref}
             initial={{ x: "-10vw", opacity: 0, scale: 0.5 }}
             animate={inView ? { x: 0, opacity: 1, scale: 1 } : { x: "-10vw", opacity: 0, scale: 0.5 }}
-            transition={{ delay: 0.5, duration: 0.4, ease: "easeInOut" }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
             whileHover={{ scale: 1.05 }}
           >
             <motion.img src={aboutMeImg} alt={name} />
