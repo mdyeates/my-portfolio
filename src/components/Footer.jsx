b797b5c011643d7b13cc0af1a592430f700b0709
Michael Yeates <michael-yeates@outlook.com>
2023-08-29 13:47:04 +0100

    Add buy me a coffee button, change hl2 color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { AiOutlineStar } from "react-icons/ai";
 import { BiGitRepoForked } from "react-icons/bi";
 import SocialIcons from "./SocialIcons";
+import { motion } from "framer-motion";
+
 const Footer = () => {
   const date = new Date();
   const currentYear = date.getFullYear();
@@ -24,19 +26,49 @@ const Footer = () => {
       .catch((e) => console.error(e));
   }, []);
 
+  const buttonVariants = {
+    hover: {
+      scale: 1.05,
+    },
+    tap: {
+      scale: 1,
+    },
+  };
+
   return (
     <footer>
       <SocialIcons />
+
       <a className="footer-link" href="https://github.com/mdyeates/my-portfolio">
         <span>
           <AiOutlineStar /> {gitHubInfo.stars} <BiGitRepoForked />
-          {gitHubInfo.forks} - Show your support by giving this project a star!
+          {gitHubInfo.forks} - Give this project a star!
         </span>
         <p>
           <span>▷</span> Designed and built by Michael Yeates &copy; {currentYear}
         </p>
         <p>All views and work are my own</p>
       </a>
+      <motion.a
+        href="https://www.buymeacoffee.com/mdyeates"
+        initial="initial"
+        whileHover="hover"
+        whileTap="tap"
+        transition={{ ease: "easeInOut" }}
+      >
+        <motion.img
+          src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png"
+          alt="Buy Me A Coffee"
+          style={{
+            height: "40px",
+            scale: 1,
+            width: "auto",
+            marginTop: "10px",
+          }}
+          variants={buttonVariants}
+          transition={{ ease: "easeInOut" }}
+        />
+      </motion.a>
     </footer>
   );
 };
